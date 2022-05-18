ac36d5557077bc3ca27c18ed51806a7c5bae61f9
Gleb <gleb1336@gmail.com>
2022-05-18 15:07:10 +0300

    Выполнено задание для второй сессии.

---
+++ Session2/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Session2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<User> userList = new List<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            using(Session1Entities pi = new Session1Entities())
+            {
+                userList = pi.User.ToList();
+            }
+            foreach(var q in userList)
+            {
+                UsersComboBox.Items.Add(q.FIO);
+            }
+        }
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SubsTextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HeaderTextBlock.Text = "Абоненты ТНС";
+        }
+
+        private void EquipTextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HeaderTextBlock.Text = "Управление оборудованием ТНС";
+        }
+
+        private void AssetsTextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HeaderTextBlock.Text = "Активы ТНС";
+        }
+
+        private void BillingTextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HeaderTextBlock.Text = "Биллинг ТНС";
+        }
+
+        private void SupprtTextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HeaderTextBlock.Text = "Поддержка пользователей ТНС";
+        }
+
+        private void crmTextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HeaderTextBlock.Text = "CRM ТНС";
+        }
+
+        private void UsersComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
